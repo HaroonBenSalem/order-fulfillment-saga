cedfe58c99a404831e810827105e2fe76ff9eb8a
Haroun Ben Salem <haaroun.bensalem@gmail.com>
2026-09-19 16:55:38 +0100

    fix(inventory-service): correct StockRepository.findByProductId parameter type to UUID

--- inventory-service/src/main/java/com/orderfulfillment/inventoryservice/repository/StockRepository.java
+++ inventory-service/src/main/java/com/orderfulfillment/inventoryservice/repository/StockRepository.java
@@ -4,7 +4,9 @@ import com.orderfulfillment.inventoryservice.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
+
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-    Optional<Stock> findByProductId(String productId);
+    Optional<Stock> findByProductId(UUID productId);
 }
