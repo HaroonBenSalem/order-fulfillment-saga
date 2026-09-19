a5fdd837d215ab0f5d4bd5a094dd0b3e22ac3886
Haroun Ben Salem <haaroun.bensalem@gmail.com>
2026-09-19 16:55:41 +0100

    fix(saga-orchestrator-service): fix created_at/updated_at column typos in SagaState entity

--- saga-orchestrator-service/src/main/java/com/orderfulfillment/sagaorchestratorservice/entity/SagaState.java
+++ saga-orchestrator-service/src/main/java/com/orderfulfillment/sagaorchestratorservice/entity/SagaState.java
@@ -26,7 +26,7 @@ public class SagaState{
     private SagaStatus status;
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
-    @Column(name = "update_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Instant updateAt;
 
     public SagaState (UUID sagaId, UUID orderId, SagaStatus status){
